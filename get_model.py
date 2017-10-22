857ea3f968871fe30ae6ed38a4fd9f9961ef8d86
Arda Mavi <ardamavi2@gmail.com>
2017-10-22 18:23:15 +0300

    Added LENET-5 and MVGG-5 models to get_model.py

--- get_model.py
+++ get_model.py
@@ -42,3 +42,70 @@ def get_model():
     print(model.summary())
 
     return model
+
+
+# Other Models:
+
+# LENET-5
+def get_lenet_5():
+    inputs = Input(shape=(28, 28, 1))
+    
+    conv_1 = Conv2D(6, (5, 5), strides=(1,1), padding='SAME')(inputs)
+    act_1 = Activation('relu')(conv_1)
+    
+    pooling_1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(act_1)
+    
+    conv_2 = Conv2D(16, (5, 5), strides=(1,1), padding='SAME')(pooling_1)
+    act_2 = Activation('relu')(conv_2)
+    
+    pooling_2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(act_2)
+    
+    conv_3 = Conv2D(120, (5, 5), strides=(1,1), padding='SAME')(pooling_2)
+    act_3 = Activation('relu')(conv_3)
+    
+    flat_1 = Flatten()(act_3)
+    
+    fc = Dense(84)(flat_1)
+    fc = Activation('relu')(fc)
+    fc = Dropout(0.5)(fc)
+    
+    fc = Dense(10)(fc)
+    outputs = Activation('softmax')(fc)
+    
+    model = Model(inputs=inputs, outputs=outputs)
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    
+    return model
+
+# MVGG-5
+def get_mvgg_5():
+    inputs = Input(shape=(28, 28, 1))
+    
+    conv_1 = Conv2D(16, (3, 3), strides=(1,1), padding='SAME')(inputs)
+    act_1 = Activation('relu')(conv_1)
+    
+    conv_2 = Conv2D(16, (3, 3), strides=(1,1), padding='SAME')(act_1)
+    act_2 = Activation('relu')(conv_2)
+    
+    pooling_1 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(act_2)
+    
+    conv_3 = Conv2D(48, (3, 3), strides=(1,1), padding='SAME')(pooling_1)
+    act_3 = Activation('relu')(conv_3)
+    
+    pooling_2 = MaxPooling2D(pool_size=(2, 2), strides=(2, 2))(act_3)
+    
+    flat_1 = Flatten()(pooling_2)
+    
+    fc = Dense(128)(flat_1)
+    fc = Activation('relu')(fc)
+    fc = Dropout(0.5)(fc)
+    
+    fc = Dense(10)(fc)
+    outputs = Activation('softmax')(fc)
+    
+    model = Model(inputs=inputs, outputs=outputs)
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    
+    return model
